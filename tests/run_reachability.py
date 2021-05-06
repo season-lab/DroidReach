ba20248e2650bd042da6aa3c21a35447344b6ba3
Luca Borzacchiello <lucaborza@gmail.com>
2021-05-06 20:17:33 +0200

    timeout decorator

--- tests/run_reachability.py
+++ tests/run_reachability.py
@@ -5,10 +5,14 @@ import os
 
 sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), ".."))
 from cex.cex import CEX
+from timeout_decorator import timeout, TimeoutError
 
 SCRIPTDIR = os.path.realpath(os.path.dirname(__file__))
 
 LIBPDFIUM_PATH  = os.path.join(SCRIPTDIR, "argus-saf/libpdfium/libmodpdfium.so")
+
+def print_stderr(*msg):
+    sys.stderr.write(" ".join(map(str, msg)) + "\n")
 
 def getExported(libpath):
     rz = rzpipe.open(libpath)
@@ -21,6 +25,7 @@ def getExported(libpath):
         res.append(export["vaddr"])
     return res
 
+@timeout(60*15)
 def getNodesCfgAngr(proj, addr):
     state = proj.factory.blank_state(mode="fastpath")
     cfg = proj.analyses.CFGEmulated(fail_fast=True, starts=[addr],
@@ -33,6 +38,7 @@ def getNodesCfgAngr(proj, addr):
     return nodes
 
 cex = CEX()
+@timeout(60*15)
 def getNodesGhidra(libpath, addr):
     cg = cex.get_callgraph(libpath, entry=addr, plugins=["Ghidra"])
 
@@ -53,9 +59,14 @@ def run(libpath):
     exported = getExported(libpath)
 
     for funaddr in exported:
-        funaddr     = funaddr + 0x400000
-        nodesAngr   = getNodesCfgAngr(proj, funaddr)
-        nodesGhidra = getNodesGhidra(libpath, funaddr)
+        funaddr = funaddr + 0x400000
+        print_stderr("Processing %#x" % funaddr)
+        try:
+            nodesAngr   = getNodesCfgAngr(proj, funaddr)
+            nodesGhidra = getNodesGhidra(libpath, funaddr)
+        except TimeoutError:
+            print_stderr("Timeout expired on %#x" % funaddr)
+            continue
 
         print("%#x, %d, %d" % (funaddr, len(nodesAngr), len(nodesGhidra)))
 
