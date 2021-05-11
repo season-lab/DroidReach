82b7a20ef1f3561ce242b4e04f5bd9d78fad7ad6
Luca Borzacchiello <lucaborza@gmail.com>
2021-05-11 18:09:04 +0200

    fixed ptr_from_java

--- tests/argus-saf/pointers_from_java.py
+++ tests/argus-saf/pointers_from_java.py
@@ -1,39 +1,79 @@
+import claripy
 import angr
 import sys
 
-from nativedroid.analyses.resolver.annotation.java_type_annotations import JobjectAnnotation
-from nativedroid.analyses.resolver.armel_resolver import ArmelResolver
+from nativedroid.analyses.resolver.model.android_app_model import JNINativeInterface, JObject
+from nativedroid.analyses.resolver.jni.java_type import get_type, get_type_size
 from nativedroid.analyses.analysis_center import AnalysisCenter
 
 MAXITER = 200
+DEBUG   = False
+
+def prepare_state(proj, addr, args):
+    state = proj.factory.blank_state(addr=addr)
+    state.regs.r0 = claripy.BVV(
+        JNINativeInterface(proj, AnalysisCenter(None, "", "")).ptr,
+        proj.arch.bits)
+    state.regs.r1 = claripy.BVV(
+        JObject(proj).ptr, proj.arch.bits)
+
+    parsed_args = dict()
+    for i, a in enumerate(args.split(",")):
+        a = a.strip().replace(" ", "")
+        parsed_args[i+2] = a
+
+    for arg_id in parsed_args:
+        arg_type = parsed_args[arg_id]
+        if arg_type not in {"long", "int"}:
+            typ       = get_type(proj, arg_type.replace('/', '.'))
+            typ_size  = get_type_size(proj, arg_type)
+            data      = claripy.BVV(typ.ptr, typ_size)
+        elif arg_type == "long":
+            data = claripy.BVS("long_arg_%d" % arg_id, proj.arch.bits)
+        else:
+            data = claripy.BVS("int_arg_%d" % arg_id, proj.arch.bits)
+
+        if arg_id < 3:
+            state.regs.__setattr__('r%d' % arg_id, data)
+        else:
+            state.stack_push(data)
+    return state
 
 def checkTaintedLoads(proj, addr, args):
-    resolver = ArmelResolver(proj, AnalysisCenter(None, "", ""))
-    state, _ = resolver.prepare_initial_state(args)
+    state = prepare_state(proj, addr, args)
 
-    state.ip = addr
-
-    tainted_load = False
+    tainted_load = list()
     def checkTaint(state):
-        global tainted_load
-        read_addr = state.inspect.mem_read_expr
-        for annotation in read_addr.annotations:
-            if isinstance(annotation, JobjectAnnotation):
-                if annotation.taint_info['is_taint']:
-                    tainted_load = True
-
+        read_addr = state.inspect.mem_read_address
+        if read_addr is None:
+            return
+        for symb_name in read_addr.variables:
+            if "long_arg_" in symb_name:
+                tainted_load.append(symb_name)
+                break
     state.inspect.b('mem_read', when=angr.BP_BEFORE, action=checkTaint)
+
+    if DEBUG:
+        print state.regs.r0
+        print state.regs.r1
+        print state.regs.r2
 
     i    = 0
     smgr = proj.factory.simgr(state, veritesting=False, save_unsat=False)
     while len(smgr.active) > 0:
-        if tainted_load or i > MAXITER:
+        if len(tainted_load) > 0 or i > MAXITER:
             break
 
+        if DEBUG:
+            for s in smgr.active:
+                print s
+                s.block().pp()
+
         smgr.explore(n=1)
+        if DEBUG: print smgr, smgr.errored, tainted_load
         i += 1
 
-    return tainted_load
+    return len(tainted_load) > 0
 
 if __name__ == "__main__":
     if len(sys.argv) < 4:
--- tests/find_ptr_from_java/analyze_apk.py
+++ tests/find_ptr_from_java/analyze_apk.py
@@ -1,3 +1,4 @@
+import subprocess
 import sys
 import os
 
@@ -5,12 +6,23 @@ sys.path.append(os.path.join(os.path.dirname(os.path.abspath(__file__)), "../.."
 from apk_analyzer import APKAnalyzer
 from cex.cex import CEX
 
+ptr_from_java = os.path.join(os.path.dirname(os.path.abspath(__file__)), "../argus-saf/run_pointers_from_java.sh")
+
 def usage():
     print("USAGE: %s <apk_path>" % sys.argv[0])
     exit(1)
 
+def does_it_use_jlong_as_ptr(lib, addr, args):
+    if "long" not in args:
+        return False
+
+    out = subprocess.check_output(
+        [ptr_from_java, lib, addr, args]
+    )
+    return b"true" in out.lower()
+
 def build_args(class_name, args):
-    return class_name + ",android.content.Context," + args.replace(" ", "")
+    return args.replace(" ", "")
 
 if __name__ == "__main__":
     if len(sys.argv) < 2:
@@ -43,11 +55,15 @@ if __name__ == "__main__":
         native_lib  = None
         native_addr = None
         for native_impl in native_impls:
-            if native_impl.analyzer.arch in {"armeabi-v7a", "armeabi"}:
+            if native_impl.analyzer.arch in {"armeabi-v7a"}:
                 native_addr = native_impl.offset
                 native_lib  = native_impl.analyzer.libpath
                 break
         if native_addr is None:
             continue
 
-        print(method_name, build_args(demangled_class_name, demangled_args), native_lib, hex(native_addr))
+        lib  = native_lib
+        addr = hex(native_addr)
+        args = build_args(demangled_class_name, demangled_args)
+
+        print(method_name, does_it_use_jlong_as_ptr(lib, addr, args))
