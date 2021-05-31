6784bccbcdfc3d3726b069c1fd1ad2e67b575651
Luca Borzacchiello <lucaborza@gmail.com>
2021-05-31 12:58:33 +0200

    c++ obj script

--- sample_cpp_obj.py
+++ sample_cpp_obj.py
@@ -1,7 +1,10 @@
+import claripy
 import sys
+import os
 
 from apk_analyzer import APKAnalyzer
 from cex.cex import CEXProject
+from cex import to_dot
 
 def usage():
     print("USAGE: %s <apk>" % sys.argv[0])
@@ -48,3 +51,20 @@ if __name__ == "__main__":
 
     for native_method, arg_ids, vtable in jlong_as_ptrs_with_constructors:
         print(native_method, arg_ids, hex(vtable))
+
+        def constructor(proj):
+            s   = proj.factory.blank_state()
+            obj = s.heap._malloc(4)
+            s.memory.store(obj, claripy.BVV(vtable, 4*8), endness=proj.arch.memory_endness)
+            s.regs.r2 = obj
+            return s
+
+        proj = CEXProject(native_method.libpath, plugins=["Ghidra", "AngrEmulated"])
+        angr_emu.set_state_constructor(native_method.offset, constructor)
+
+        cg = proj.get_callgraph(native_method.offset)
+
+        dot_str = to_dot(cg)
+        with open("/dev/shm/graph.dot", "w") as fout:
+            fout.write(dot_str)
+        os.system("xdot /dev/shm/graph.dot")
