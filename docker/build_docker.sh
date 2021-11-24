f0e42a73b7febc75f981859fcfda627f68a4c9c7
Luca Borzacchiello <lucaborza@gmail.com>
2021-11-24 13:17:22 +0100

    Dockerfile fixes

--- docker/build_docker.sh
+++ docker/build_docker.sh
@@ -1,3 +1,6 @@
 #!/bin/bash
 
-docker build . -t android-paths
+docker build -t android-paths           \
+    --build-arg USER_ID=$(id -u $USER)  \
+    --build-arg GROUP_ID=$(id -g $USER) \
+    .
